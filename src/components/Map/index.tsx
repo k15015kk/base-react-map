31a1211b85214136d8a07cfe98ec64e320b3eafb
k15015kk <aitk15015kk@outlook.com>
2022-04-28 01:03:47 +0900

    路線を地図上に描画する実装

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,25 +1,39 @@
-import ReactMapGL, { NavigationControl } from 'react-map-gl';
-import maplibregl from 'maplibre-gl'
-import { useState } from 'react';
+import ReactMapGL, { NavigationControl } from "react-map-gl";
+import maplibregl from "maplibre-gl";
+import DeckGL from "deck.gl";
+import { useState } from "react";
+import { GeoJsonLayer } from "deck.gl";
+import { getOrdinalScale } from "@deck.gl/aggregation-layers/utils/scale-utils";
 
 const Map = () => {
-    const [viewState, setViewState] = useState({
-        longitude: 139.7671365841117,
-        latitude: 35.68143950212949, 
-        zoom: 15,
-    })
+  const jsonData = require("@/data/N02-20_RailroadSection.json");
 
-    return (
-        <ReactMapGL
-            {...viewState}
-            style={{width: '100vw', height: '100vh'}}
-            mapStyle={process.env.MAP_URL}
-            mapLib={maplibregl}
-            onMove={(event) => setViewState(event.viewState)}
-        >
-            <NavigationControl />
-        </ReactMapGL>
-    )
-}
+  const [viewState, setViewState] = useState({
+    longitude: 139.7671365841117,
+    latitude: 35.68143950212949,
+    zoom: 15,
+  });
+
+  const geoJsonLayer = new GeoJsonLayer({
+    id: "geojson",
+    data: jsonData,
+    lineWidthMinPixels: 4,
+    getLineColor: [69, 90, 100, 255],
+  });
+
+  return (
+    <DeckGL
+      initialViewState={viewState}
+      layers={[geoJsonLayer]}
+      style={{ width: "100vw", height: "100vh" }}
+      controller={true}
+      onViewStateChange={(event) => setViewState(event.viewState)}
+    >
+      <ReactMapGL mapStyle={process.env.MAP_URL} mapLib={maplibregl}>
+        <NavigationControl />
+      </ReactMapGL>
+    </DeckGL>
+  );
+};
 
 export default Map;
